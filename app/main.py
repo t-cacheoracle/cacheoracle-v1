3e3cd74a81f38ad30ed7134286939c5d81d98fe2
Adrian <adrrii.joseph07@gmail.com>
2026-03-21 20:55:15 -0700

    Updated grpc call

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import os
 from typing import cast
 
+import grpc
+import cacheoracle_pb2
+import cacheoracle_pb2_grpc
+
 from fastapi import FastAPI
 from uagents_core.contrib.protocols.chat import (
     ChatMessage,
@@ -10,12 +14,11 @@ from uagents_core.envelope import Envelope
 from uagents_core.identity import Identity
 from uagents_core.utils.messages import parse_envelope, send_message_to_agent
 
-name = "Chat Protocol Adapter"
+name = "FlashAI"
 from dotenv import load_dotenv
 
 load_dotenv()
 identity = Identity.from_seed(os.getenv("AGENT_SEED_PHRASE"), 0)
-readme = "# Chat Protocol Adapter \nExample of how to integrate chat protocol."
 
 app = FastAPI()
 
@@ -26,9 +29,24 @@ async def healthcheck():
 @app.post("/chat")
 async def handle_message(env: Envelope):
     msg = cast(ChatMessage, parse_envelope(env, ChatMessage))
-    print(f"Received message from {env.sender}: {msg.text()}")
+    user_text = msg.text()
+
+    print(f"Received message from {env.sender}: {user_text}")
+
+    # call (gRPC)
+    backend_reply = call_backend(user_text)
+
     send_message_to_agent(
         destination=env.sender,
-        msg=ChatMessage([TextContent("Thanks for the message!")]),
+        msg=ChatMessage([TextContent(backend_reply)]),
         sender=identity,
     )
+
+def call_backend(user_input: str) -> str:
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = cacheoracle_pb2_grpc.CodegenServiceStub(channel)
+
+        request = cacheoracle_pb2.GenerateRequest(prompt=user_input)
+        response = stub.GeneratePython(request)
+
+        return response.python_code
